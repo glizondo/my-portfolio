4b59fb4ed3de5ee3ddd6b961f52efd9e23910444
unknown <guilizondo@gmail.com>
2024-10-28 16:26:15 -0700

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Header from "./components/Header";
+import ReactGA from "react-ga4";
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import FunFactBody from "./components/FunFact/FunFactBody";
+import Header from "./components/Header";
+
+const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID;
+ReactGA.initialize(GA_MEASUREMENT_ID);
 
 function App() {
+  ReactGA.send("pageview");
+
   const [selectedPage, setSelectedPage] = useState("about");
   const handleBodyChange = (page) => {
     setSelectedPage(page);
@@ -19,7 +29,10 @@ function App() {
           <Route path="/" element={<Navigate to="/about" replace />} />
           <Route path="/about" element={<Body bodyName={selectedPage} />} />
           <Route path="/projects" element={<Body bodyName={selectedPage} />} />
-          <Route path="/photography" element={<Body bodyName={selectedPage} />} />
+          <Route
+            path="/photography"
+            element={<Body bodyName={selectedPage} />}
+          />
         </Routes>
       </div>
       <Footer />
